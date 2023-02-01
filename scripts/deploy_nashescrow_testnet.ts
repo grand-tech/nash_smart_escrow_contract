a9f227b62869b17207852a9d713944dbd0d6095c
allen <allenmuhani@gmail.com>
2023-02-01 14:47:56 +0300

    fixed contract deployment logic

--- scripts/deploy_nashescrow_testnet.ts
+++ scripts/deploy_nashescrow_testnet.ts
@@ -3,17 +3,9 @@ import { ethers, upgrades } from "hardhat";
 // TODO: pluck out some code from environment variables.
 async function main() {
   const NashEscrow = await ethers.getContractFactory("NashEscrow");
-  const nashEscrow = await upgrades.deployProxy(
-    NashEscrow,
-    [
-      "0xF1a91486c8174168f03b88F7F36c425d53da3688",
-      1000000000000,
-      1000000000000,
-    ],
-    {
-      initializer: "initialize",
-    }
-  );
+  const nashEscrow = await upgrades.deployProxy(NashEscrow, [], {
+    initializer: "initialize",
+  });
 
   await nashEscrow.deployed();
   console.log("NashEscrow deployed to: { ", nashEscrow.address, " } ");
